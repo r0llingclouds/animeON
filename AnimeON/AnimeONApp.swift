a95b12fbdca0210426764cea725e3909a97f8239
tirso <tir303@icloud.com>
2025-04-26 13:37:49 +0200

    Initial Commit

---
+++ AnimeON/AnimeONApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeONApp.swift
+//  AnimeON
+//
+//  Created by Tirso López Ausens on 26/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimeONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimeON/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimeON
+//
+//  Created by Tirso López Ausens on 26/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
